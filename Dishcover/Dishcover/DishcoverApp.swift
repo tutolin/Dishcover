e166904dfb81cff8ec56cd206d04d60591b12686
Tolulope Aboyeji <aboyejitolu@gmail.com>
2024-01-30 22:11:57 -0500

    created a new swifui project

---
+++ Dishcover/Dishcover/DishcoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  DishcoverApp.swift
+//  Dishcover
+//
+//  Created by Tolulope Aboyeji on 30/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct DishcoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
