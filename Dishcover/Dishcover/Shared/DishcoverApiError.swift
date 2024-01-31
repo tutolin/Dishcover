c54c98979f305bf016bb8ce1ffa064d11ebc6c9e
Tolulope Aboyeji <aboyejitolu@gmail.com>
2024-01-30 22:24:41 -0500

    Add Network and Api Error class

---
+++ Dishcover/Dishcover/Shared/DishcoverApiError.swift
@@ -0,0 +1,8 @@
+//
+//  DishcoverApiError.swift
+//  Dishcover
+//
+//  Created by Tolulope Aboyeji on 30/01/2024.
+//
+
+import Foundation
---
+++ Dishcover/Dishcover/Shared/HTTPDataDownloader.swift
@@ -0,0 +1,8 @@
+//
+//  HTTPDataDownloader.swift
+//  Dishcover
+//
+//  Created by Tolulope Aboyeji on 30/01/2024.
+//
+
+import Foundation
